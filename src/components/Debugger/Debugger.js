153d62c9860180a15b5fc542a3895c35ca68000e
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-14 14:26:01 +0700

    add basic tabs header for debugger

--- src/components/Debugger/Debugger.js
+++ src/components/Debugger/Debugger.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Tabs from 'react-bulma-components/lib/components/tabs';
 
 const Debugger = () => (
   <>
+    <Tabs type={'toggle'} fullwidth={true}>
+      <Tabs.Tab>General</Tabs.Tab>
+      <Tabs.Tab>Disassembly</Tabs.Tab>
+    </Tabs>
   </>
 );
 
