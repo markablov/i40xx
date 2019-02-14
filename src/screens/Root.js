289ac735ec6c5c120a45116d38c06425575423bd
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-14 14:47:02 +0700

    put main application sections into boxes

--- src/screens/Root.js
+++ src/screens/Root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Section from 'react-bulma-components/lib/components/section';
 import Container from 'react-bulma-components/lib/components/container';
 import Columns from 'react-bulma-components/lib/components/columns';
+import Box from 'react-bulma-components/lib/components/box';
 
 import Editor from '../components/Editor/Editor.js';
 import Debugger from '../components/Debugger/Debugger.js';
@@ -13,10 +14,14 @@ const Root = () => (
     <Container>
       <Columns>
         <Columns.Column>
-          <Editor />
+          <Box>
+            <Editor />
+          </Box>
         </Columns.Column>
         <Columns.Column>
-          <Debugger />
+          <Box>
+            <Debugger />
+          </Box>
         </Columns.Column>
       </Columns>
     </Container>
