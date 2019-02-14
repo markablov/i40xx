ae6106dd4f0fda6c1c35132915fd976e20aa35ce
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-14 14:06:42 +0700

    use bulma css framework

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Section from 'react-bulma-components/lib/components/section';
+import Container from 'react-bulma-components/lib/components/container';
+
 const App = () => (
-  <div className="App">
-  </div>
+  <Section>
+    <Container>
+    </Container>
+  </Section>
 );
 
 export default App;
