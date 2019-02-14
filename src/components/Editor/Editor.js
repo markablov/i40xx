87eff5faa102a12cb974dd2b699c02c9cfa8eb5e
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-14 14:49:43 +0700

    embed ace editor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import AceEditor from 'react-ace';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
 const Editor = () => (
   <>
+    <AceEditor mode="javascript" theme="monokai" name="editor" width="auto" />
   </>
 );
 
