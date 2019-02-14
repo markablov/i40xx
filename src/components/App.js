19adcd515655a05ae55f992a8311f6e762fb0d27
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-14 14:14:52 +0700

    two-column design of application

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
 import Section from 'react-bulma-components/lib/components/section';
 import Container from 'react-bulma-components/lib/components/container';
+import Columns from 'react-bulma-components/lib/components/columns';
+
+import Editor from './Editor.js';
+import Debugger from './Debugger.js';
 
 const App = () => (
   <Section>
     <Container>
+      <Columns>
+        <Columns.Column>
+          <Editor />
+        </Columns.Column>
+        <Columns.Column>
+          <Debugger />
+        </Columns.Column>
+      </Columns>
     </Container>
   </Section>
 );
