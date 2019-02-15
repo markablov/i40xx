d6543129e3c6b7b7115568970c3766d215426ae2
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-15 16:16:10 +0700

    use custom gutter render for editor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -3,11 +3,23 @@ import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
+import GutterRenderer from './GutterRenderer.js';
+
 class Editor extends Component {
   editorRef = React.createRef();
 
   get editor(){
     return this.editorRef.current.editor;
+  }
+
+  componentDidMount() {
+    const editor = this.editor;
+
+    // we want to show ROM offset for instructions, so need to change
+    // gutter with line numbers to custom renderer
+    editor.renderer.$gutterLayer.$renderer = GutterRenderer;
+    editor.on('changeSelection', GutterRenderer.update);
+    GutterRenderer.update(null, editor);
   }
 
   render(){
