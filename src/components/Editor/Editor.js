4cb3fbc12379bff3aa8cbf2066d3b2fdec838cde
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-15 15:57:41 +0700

    convert Editor component from function-based to class-based

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
-const Editor = () => (
-  <>
-    <AceEditor mode="javascript" theme="monokai" name="editor" width="auto" />
-  </>
-);
+class Editor extends Component {
+  render(){
+    return (
+      <>
+        <AceEditor mode="javascript" theme="monokai" name="editor" width="auto" />
+      </>
+    );
+  }
+}
 
 export default Editor;
