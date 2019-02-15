c9a5fd6800129b9932b5e01af321fbc2efea5f0c
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-15 10:02:53 +0700

    make debugger tabs work

--- src/components/Debugger/Debugger.js
+++ src/components/Debugger/Debugger.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import Tabs from 'react-bulma-components/lib/components/tabs';
 
+import Memory from './Memory.js';
+import General from './General.js';
+
 class Debugger extends Component {
+  state = {
+    activeTab: 0,
+    tabs: [
+      { label: 'General', component: General },
+      { label: 'Memory', component: Memory }
+    ]
+  };
+
+  handleTabClick = idx => this.setState({ activeTab: idx });
+
   render(){
+    const { tabs, activeTab } = this.state;
+
     return (
       <>
         <Tabs type={'toggle'} fullwidth={false}>
-          <Tabs.Tab>General</Tabs.Tab>
-          <Tabs.Tab>Memory</Tabs.Tab>
+          {
+            tabs.map(({ label }, idx) =>
+              <Tabs.Tab active={idx === activeTab} onClick={() => this.handleTabClick(idx)} key={`debugger-tab-${idx}`}>{label}</Tabs.Tab>
+            )
+          }
         </Tabs>
+        { React.createElement(tabs[activeTab].component) }
       </>
     );
   }
