5da01dd590f25eeb49cf8bb824765eba1876e3f5
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-15 16:01:53 +0700

    add getter to receive AceEditor instance for Editor component

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -4,10 +4,16 @@ import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
 class Editor extends Component {
+  editorRef = React.createRef();
+
+  get editor(){
+    return this.editorRef.current.editor;
+  }
+
   render(){
     return (
       <>
-        <AceEditor mode="javascript" theme="monokai" name="editor" width="auto" />
+        <AceEditor mode="javascript" theme="monokai" name="editor" width="auto" ref={this.editorRef} />
       </>
     );
   }
