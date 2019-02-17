95a7c8abae09bdda778c1e4c77d8d5f1a525df1d
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-17 19:36:00 +0700

    track of offset has been changed due more recent update() call

--- src/components/Editor/OffsetCalculator.js
+++ src/components/Editor/OffsetCalculator.js
@@ -1,8 +1,13 @@
 class OffsetCalculator {
   _offsets = [];
+  _changedFromLastUpdate = false;
 
   get offsets() {
     return this._offsets;
+  }
+
+  get updated() {
+    return this._changedFromLastUpdate;
   }
 
   update(){
