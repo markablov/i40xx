2bba7864b601acd261bb0f62425ed42bfee5ed42
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-18 10:54:12 +0700

    highlight instructions

--- src/components/Editor/AssemblyMode/HighlightRules.js
+++ src/components/Editor/AssemblyMode/HighlightRules.js
@@ -4,8 +4,19 @@ class HighlightRules extends window.ace.acequire('ace/mode/text_highlight_rules'
 
     this.$rules = {
       start: [
-        { token: 'comment', regex: '(//|#).*$' },
-        { token: 'entity.name.function', regex: '^\\w+:' },
+        {
+          token: 'comment',
+          regex: '(//|#).*$'
+        },
+        {
+          token: 'entity.name.function',
+          regex: '^\\w+:'
+        },
+        {
+          token: 'keyword.control',
+          regex: '\\b(nop|ldm|ld|xch|add|sub|inc|bbl|jin|src|fin|jun|jms|jcn|isz|fim|rdm|rd0|rd1|rd2|rd3|rdr|wrm|wr0|wr1|wr2|wr3|wrr|wmp|adm|sbm|clb|clc|cmc|stc|cma|iac|dac|ral|rar|tcc|daa|tcs|kbp|dcl)\\b',
+          caseInsensitive: true
+        }
       ]
     };
 
