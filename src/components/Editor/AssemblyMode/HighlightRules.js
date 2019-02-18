79f1c19d503f630ef9b7a8ee7a854ce253f79969
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-18 10:30:31 +0700

    add skeleton for highlight rules

--- src/components/Editor/AssemblyMode/HighlightRules.js
+++ src/components/Editor/AssemblyMode/HighlightRules.js
@@ -1,6 +1,13 @@
 class HighlightRules extends window.ace.acequire('ace/mode/text_highlight_rules').TextHighlightRules {
   constructor() {
     super();
+
+    this.$rules = {
+      start: [
+      ]
+    };
+
+    this.normalizeRules();
   }
 }
 
