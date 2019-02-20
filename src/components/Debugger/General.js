69106565c96eb92ae492742c316c47ecd3ebfd6d
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-20 13:56:30 +0700

    add dummy handler to "Build" button

--- src/components/Debugger/General.js
+++ src/components/Debugger/General.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Button from 'react-bulma-components/lib/components/button';
 
 class General extends Component {
+  handleBuild = () => {
+  };
+
   render() {
     return (
       <>
-        <Button color="warning">Build</Button>
+        <Button color="warning" onClick={this.handleBuild}>Build</Button>
       </>
     );
   }
