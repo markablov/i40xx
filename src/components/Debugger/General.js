74bf7ebac41c55bbacbf7b0788c8f46c4a354fdf
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-20 13:54:17 +0700

    add button to compile code

--- src/components/Debugger/General.js
+++ src/components/Debugger/General.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Button from 'react-bulma-components/lib/components/button';
 
 const General = () => (
   <>
+    <Button color="warning">Build</Button>
   </>
 );
 
