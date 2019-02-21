74ef90db58ee8eb6e8c0588830307062fc5d5067
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-21 11:45:44 +0700

    allow to access redux store outside react components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 import Root from './screens/Root.js';
-import reducer from './redux/reducers/root.js';
-
-const store = createStore(reducer);
+import store from './redux/store.js';
 
 ReactDOM.render(
   <Provider store={store}>
