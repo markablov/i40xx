bffcbf9921dda8efa811f5602f1f6d336875b1c9
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-21 11:51:32 +0700

    dispatch startCompilation action on compile() call

--- src/services/compiler.js
+++ src/services/compiler.js
@@ -1,9 +1,13 @@
+import store from '../redux/store.js';
+import startCompilation from '../redux/actions/startCompilation.js';
+
 const worker = new Worker('../workers/compiler/compiler.js');
 
 worker.onmessage = () => {
 };
 
 const compile = () => {
+  store.dispatch(startCompilation());
 };
 
 export { compile };
