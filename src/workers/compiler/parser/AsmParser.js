9bf742678b0feff0ca85d6a476510ad515633325
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-21 15:18:39 +0700

    parse simple instructions without arguments

--- src/workers/compiler/parser/AsmParser.js
+++ src/workers/compiler/parser/AsmParser.js
@@ -20,7 +20,40 @@ class AsmParser extends Parser {
       });
     });
 
-    $.RULE('instruction', () => $.CONSUME(Tokens.Text));
+    $.RULE('instruction', () => {
+      $.OR([
+        { ALT: () => $.CONSUME(Tokens.InstructionNOP) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRDM) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRD0) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRD1) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRD2) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRD3) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRDR) },
+        { ALT: () => $.CONSUME(Tokens.InstructionWRM) },
+        { ALT: () => $.CONSUME(Tokens.InstructionWR0) },
+        { ALT: () => $.CONSUME(Tokens.InstructionWR1) },
+        { ALT: () => $.CONSUME(Tokens.InstructionWR2) },
+        { ALT: () => $.CONSUME(Tokens.InstructionWR3) },
+        { ALT: () => $.CONSUME(Tokens.InstructionWRR) },
+        { ALT: () => $.CONSUME(Tokens.InstructionWMP) },
+        { ALT: () => $.CONSUME(Tokens.InstructionADM) },
+        { ALT: () => $.CONSUME(Tokens.InstructionSBM) },
+        { ALT: () => $.CONSUME(Tokens.InstructionCLB) },
+        { ALT: () => $.CONSUME(Tokens.InstructionCLC) },
+        { ALT: () => $.CONSUME(Tokens.InstructionCMC) },
+        { ALT: () => $.CONSUME(Tokens.InstructionSTC) },
+        { ALT: () => $.CONSUME(Tokens.InstructionCMA) },
+        { ALT: () => $.CONSUME(Tokens.InstructionIAC) },
+        { ALT: () => $.CONSUME(Tokens.InstructionDAC) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRAL) },
+        { ALT: () => $.CONSUME(Tokens.InstructionRAR) },
+        { ALT: () => $.CONSUME(Tokens.InstructionTCC) },
+        { ALT: () => $.CONSUME(Tokens.InstructionDAA) },
+        { ALT: () => $.CONSUME(Tokens.InstructionTCS) },
+        { ALT: () => $.CONSUME(Tokens.InstructionKBP) },
+        { ALT: () => $.CONSUME(Tokens.InstructionDCL) }
+      ]);
+    });
 
     $.performSelfAnalysis();
   }
--- src/workers/compiler/parser/tokens.js
+++ src/workers/compiler/parser/tokens.js
@@ -7,6 +7,13 @@ const addToken = (name, options) => tokens[name] = createToken({ name, ...option
 addToken('NewLine', { pattern: /\r?\n/ });
 addToken('Comment', { pattern: /(?:#|(?:\/\/))[^\n\r]*/, group: Lexer.SKIPPED });
 addToken('WhiteSpace', { pattern: /\s+/, group: Lexer.SKIPPED });
-addToken('Text', { pattern: /[^\r\n]+/ });
+
+const instructions0 = [
+  'NOP', 'RDM', 'RD0', 'RD1', 'RD2', 'RD3', 'RDR', 'WRM', 'WR0', 'WR1', 'WR2',
+  'WR3', 'WRR', 'WMP', 'ADM', 'SBM', 'CLB', 'CLC', 'CMC', 'STC', 'CMA', 'IAC',
+  'DAC', 'RAL', 'RAR', 'TCC', 'DAA', 'TCS', 'KBP', 'DCL'
+];
+
+instructions0.forEach(name => addToken(`Instruction${name}`, { pattern: new RegExp(name, 'i') }));
 
 export default tokens;
