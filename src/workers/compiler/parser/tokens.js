ccf087e4fff1c0ed71c1aeba453a2ccfd8dde3da
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-22 11:10:20 +0700

    extend lexer to parse 12-bit data
    On lexer stage we can't differentiate when we want to receive 4-bit data
    token, 8-bit data token or 12-bit address token, should be checked
    by code generator

--- src/workers/compiler/parser/tokens.js
+++ src/workers/compiler/parser/tokens.js
@@ -23,8 +23,9 @@ addToken('Register', { pattern: /rr(?:(?:1[0-5])|(?:0?\d))/ });
 
 addToken('RegisterPair', { pattern: /r[0-7]/ });
 
-// [100..199] OR [200..249] OR [250..255] OR [0x00..0xff] OR [00..99]
-addToken('Data', { pattern: /(?:[01]\d\d)|(?:2[0-4]\d)|(?:25[0-5])|(?:0x[0-9a-f]{1,2})|(?:\d{1,2})/ });
+// Data is 12-bit maximum, so it could accept 0x000..0xFFF and 0..4095 ranges
+// [1000..3999] OR [4000..4089] OR [4090..4095] OR [0x000..0xFFF] OR [000..999]
+addToken('Data', { pattern: /(?:[0-3]\d\d\d)|(?:40[0-8]\d)|(?:409[0-5])|(?:0x[0-9a-f]{1,3})|(?:\d{1,3})/ });
 
 // important to define label name after keywords, because lexer tries to match first rule from array
 // and it could match label first because patterns of instruction names and labels are intersected
