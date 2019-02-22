06b91ec536142051d2cf2c052b44dfb251d3fdd3
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-22 09:23:17 +0700

    handle lexer errors

--- src/workers/compiler/parser/parser.js
+++ src/workers/compiler/parser/parser.js
@@ -2,7 +2,10 @@ import AsmLexer from './AsmLexer.js';
 import AsmParser from './AsmParser.js';
 
 const parse = sourceCode => {
-  const { tokens } = AsmLexer.tokenize(sourceCode.toLowerCase());
+  const { tokens, errors: lexerErrors } = AsmLexer.tokenize(sourceCode.toLowerCase());
+  if (lexerErrors.length)
+    return { tokens, errors: lexerErrors };
+
   AsmParser.input = tokens;
   const ast = AsmParser.program();
   return { tokens, ast, errors: AsmParser.errors };
