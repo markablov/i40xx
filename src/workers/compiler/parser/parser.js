af10115ac9c2e658feaa7875eaf3e915d6cd1459
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-23 20:13:26 +0700

    no need to return list of tokens by parser, it's internal

--- src/workers/compiler/parser/parser.js
+++ src/workers/compiler/parser/parser.js
@@ -4,11 +4,11 @@ import AsmParser from './AsmParser.js';
 const parse = sourceCode => {
   const { tokens, errors: lexerErrors } = AsmLexer.tokenize(sourceCode.toLowerCase());
   if (lexerErrors.length)
-    return { tokens, errors: lexerErrors };
+    return { errors: lexerErrors };
 
   AsmParser.input = tokens;
   const data = AsmParser.program();
-  return { tokens, data, errors: AsmParser.errors };
+  return { data, errors: AsmParser.errors };
 };
 
 export default parse;
