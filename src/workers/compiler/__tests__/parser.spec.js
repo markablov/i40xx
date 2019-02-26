43cdf6af523f62f0ff1aeb272a762deb7ffac353
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-26 07:24:34 +0700

    match lexer errors in tests

--- src/workers/compiler/__tests__/parser.spec.js
+++ src/workers/compiler/__tests__/parser.spec.js
@@ -6,7 +6,7 @@ const matchParseResults = (source, data, error) => {
   const { errors: parseErrors, data: parseData } = parse(source);
   if (error) {
     expect(parseErrors).toHaveLength(1);
-    expect(parseErrors[0].toString()).toEqual(error);
+    expect(parseErrors[0].message).toEqual(error);
   } else
     expect(parseData).toEqual(new Uint8Array(data));
 };
@@ -27,7 +27,9 @@ describe('One-byte instructions without arguments', () => {
 
 test('Undefined label', () => matchParseResults('jun unknown_label', null, 'MismatchedTokenException: Error: Unknown label unknown_label'));
 
-test('Duplicated label definition', () => matchParseResults('label: nop\nlabel: nop', null, 'MismatchedTokenException: Duplicated definition for label'));
+test('Undefined label', () => matchParseResults('jun unknown_label', null, 'Error: Unknown label unknown_label'));
+
+test('Duplicated label definition', () => matchParseResults('label: nop\nlabel: nop', null, 'Duplicated definition for label'));
 
 describe('Instructions with register as argument', () => {
   test('LD instruction', () => matchParseResults('ld rr1', [0xA1]));
@@ -35,33 +37,33 @@ describe('Instructions with register as argument', () => {
   test('ADD instruction', () => matchParseResults('add rr3', [0x83]));
   test('SUB instruction', () => matchParseResults('sub rr4', [0x94]));
   test('INC instruction', () => matchParseResults('inc rr15', [0x6F]));
-  test('incorrect register', () => matchParseResults('inc rr16', null, 'NotAllInputParsedException: Redundant input, expecting EOF but found: 6'));
-  test('incorrect argument', () => matchParseResults('add 2', null, 'MismatchedTokenException: Expecting token of type --> Register <-- but found --> \'2\' <--'));
-  test('missing argument', () => matchParseResults('ld', null, 'MismatchedTokenException: Expecting token of type --> Register <-- but found --> \'\' <--'));
+  test('incorrect register', () => matchParseResults('inc rr16', null, 'Redundant input, expecting EOF but found: 6'));
+  test('incorrect argument', () => matchParseResults('add 2', null, 'Expecting token of type --> Register <-- but found --> \'2\' <--'));
+  test('missing argument', () => matchParseResults('ld', null, 'Expecting token of type --> Register <-- but found --> \'\' <--'));
 });
 
 describe('Instructions with register pair as argument', () => {
   test('JIN instruction', () => matchParseResults('jin r1', [0x33]));
   test('SRC instruction', () => matchParseResults('src r2', [0x25]));
   test('FIN instruction', () => matchParseResults('fin r3', [0x36]));
-  test('incorrect register', () => matchParseResults('src r9', null, 'MismatchedTokenException: Expecting token of type --> RegisterPair <-- but found --> \'r9\' <--'));
+  test('incorrect register', () => matchParseResults('src r9', null, 'Expecting token of type --> RegisterPair <-- but found --> \'r9\' <--'));
 });
 
 describe('Instructions with 4-bit data as argument', () => {
   test('BBL instruction', () => matchParseResults('bbl 10', [0xCA]));
   test('LDM instruction', () => matchParseResults('ldm 0xF', [0xDF]));
-  test('too big value', () => matchParseResults('ldm 17', null, 'MismatchedTokenException: Error: Argument is too big, should be 0xF or less'));
+  test('too big value', () => matchParseResults('ldm 17', null, 'Error: Argument is too big, should be 0xF or less'));
 });
 
 describe('FIM instruction', () => {
   test('valid', () => matchParseResults('fim r0, 0xAA', [0x20, 0xAA]));
-  test('too big value', () => matchParseResults('fim r0, 260', null, 'MismatchedTokenException: Error: Argument is too big, should be 0xFF or less'));
+  test('too big value', () => matchParseResults('fim r0, 260', null, 'Error: Argument is too big, should be 0xFF or less'));
 });
 
 describe('Instructions with 12-bit address as argument', () => {
   test('JUN instruction', () => matchParseResults('jun 0xABC', [0x4A, 0xBC]));
   test('JMS instruction', () => matchParseResults('jms 09:0xFA', [0x59, 0xFA]));
   test('JMS instruction with label', () => matchParseResults('jms label\nlabel: nop', [0x50, 0x02, 0x00]));
-  test('too big value', () => matchParseResults('jun 4096', null, 'NotAllInputParsedException: Redundant input, expecting EOF but found: 6'));
-  test('too big value with bank address', () => matchParseResults('jun 17:0x00', null, 'NotAllInputParsedException: Redundant input, expecting EOF but found: :'));
+  test('too big value', () => matchParseResults('jun 4096', null, 'Redundant input, expecting EOF but found: 6'));
+  test('too big value with bank address', () => matchParseResults('jun 17:0x00', null, 'Redundant input, expecting EOF but found: :'));
 });
