ad44e5d97ecdbe5eb4cb8a74f9269c6ecd423a01
Mark Ablovatskii <mark.ablov@gmail.com>
2019-02-26 14:13:39 +0700

    send information about errors from compiler worker

--- src/workers/compiler/compiler.js
+++ src/workers/compiler/compiler.js
@@ -1,6 +1,10 @@
 import parse from './parser/parser.js';
 
 onmessage = ({ data: sourceCode }) => {
-  const { data } = parse(sourceCode);
-  postMessage({ dump: data });
+  const { data, errors } = parse(sourceCode);
+
+  postMessage({
+    dump: data,
+    errors: errors.map(({ message, token, line }) => ({ message, row: token ? token.startLine : line }))
+  });
 };
