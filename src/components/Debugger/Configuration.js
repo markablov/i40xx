41bd57328b617a75a39f6dbb3d50c335d975b337
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-01 11:10:08 +0700

    basic layout for configuration tab

--- src/components/Debugger/Configuration.js
+++ src/components/Debugger/Configuration.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import Card from 'react-bulma-components/lib/components/card';
+import Image from 'react-bulma-components/lib/components/image';
+import Columns from 'react-bulma-components/lib/components/columns';
+
 const Configuration = () => (
   <>
+    <Columns>
+      <Columns.Column>
+        <Card>
+          <Card.Header>
+            <Card.Header.Title>CPU</Card.Header.Title>
+          </Card.Header>
+          <Card.Content>
+            <Image
+              src="https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552&format=original"
+              size={128}
+            />
+          </Card.Content>
+        </Card>
+      </Columns.Column>
+    </Columns>
   </>
 );
 
