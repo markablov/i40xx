3005ca5452f0cf03f84c60db241e818ad390c4e7
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-05 14:23:06 +0700

    convert Configuration component from function-based to class-based

--- src/components/Debugger/Configuration/Configuration.js
+++ src/components/Debugger/Configuration/Configuration.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from 'react-bulma-components/lib/components/card';
 import Image from 'react-bulma-components/lib/components/image';
 import Columns from 'react-bulma-components/lib/components/columns';
@@ -8,44 +8,48 @@ import { Field, Control, Input } from 'react-bulma-components/lib/components/for
 import CPU4004 from './CPU4004.svg';
 import ROM4001 from './ROM4001.svg';
 
-const Configuration = () => (
-  <>
-    <Columns>
-      <Columns.Column>
-        <Card>
-          <Card.Header>
-            <Card.Header.Title>CPU</Card.Header.Title>
-          </Card.Header>
-          <Card.Content>
-            <Button.Group hasAddons={true}>
-              <Button isSelected={true} color="info">i4004</Button>
-              <Button disabled={true}>i4040</Button>
-            </Button.Group>
-            <Image src={CPU4004} size={128} />
-          </Card.Content>
-        </Card>
-      </Columns.Column>
-      <Columns.Column>
-        <Card>
-          <Card.Header>
-            <Card.Header.Title>ROM</Card.Header.Title>
-          </Card.Header>
-          <Card.Content>
-            <Button.Group hasAddons={true}>
-              <Button isSelected={true} color="info">i4001</Button>
-            </Button.Group>
-            <Image src={ROM4001} size={128} />
-            <Field>
-              <Control>
-                <Input placeholder="Amount of ROMs" onChange={() => 0} size="small"/>
-              </Control>
-            </Field>
-          </Card.Content>
-        </Card>
-      </Columns.Column>
-    </Columns>
-    <Button color="success">Reconfigure</Button>
-  </>
-);
+class Configuration extends Component {
+  render(){
+    return (
+      <>
+        <Columns>
+          <Columns.Column>
+            <Card>
+              <Card.Header>
+                <Card.Header.Title>CPU</Card.Header.Title>
+              </Card.Header>
+              <Card.Content>
+                <Button.Group hasAddons={true}>
+                  <Button isSelected={true} color="info">i4004</Button>
+                  <Button disabled={true}>i4040</Button>
+                </Button.Group>
+                <Image src={CPU4004} size={128} />
+              </Card.Content>
+            </Card>
+          </Columns.Column>
+          <Columns.Column>
+            <Card>
+              <Card.Header>
+                <Card.Header.Title>ROM</Card.Header.Title>
+              </Card.Header>
+              <Card.Content>
+                <Button.Group hasAddons={true}>
+                  <Button isSelected={true} color="info">i4001</Button>
+                </Button.Group>
+                <Image src={ROM4001} size={128} />
+                <Field>
+                  <Control>
+                    <Input placeholder="Amount of ROMs" onChange={() => 0} size="small"/>
+                  </Control>
+                </Field>
+              </Card.Content>
+            </Card>
+          </Columns.Column>
+        </Columns>
+        <Button color="success">Reconfigure</Button>
+      </>
+    );
+  }
+}
 
 export default Configuration;
