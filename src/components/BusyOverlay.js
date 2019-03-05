fc0c830b00aeee4a73c23e96fc83c3304c93c736
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-05 14:18:38 +0700

    show overlay on reconfiguration

--- src/components/BusyOverlay.js
+++ src/components/BusyOverlay.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import LoadingOverlay from 'react-loading-overlay';
 
-const BusyOverlay = ({ children, isCompiling }) => (
-  <LoadingOverlay active={isCompiling} spinner text='Compiling...'>
+const BusyOverlay = ({ children, isCompiling, isConfigurating }) => (
+  <LoadingOverlay active={isCompiling || isConfigurating} spinner text={isCompiling ? 'Compiling...' : 'Configurating...'}>
     {children}
   </LoadingOverlay>
 );
 
-export default connect(state => ({ isCompiling: state.compiling }))(BusyOverlay);
+export default connect(state => ({ isCompiling: state.compiling, isConfigurating: state.configurating }))(BusyOverlay);
