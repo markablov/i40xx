2ac87ab7a06be6b448a9c6cca427d84bf73be921
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-13 13:36:05 +0700

    command-oriented architecture for emulator worker communications

--- src/workers/emulator/emulator.js
+++ src/workers/emulator/emulator.js
@@ -1,2 +1,17 @@
-onmessage = () => {
+const commands = {
+  configure: () => {
+  }
+};
+
+onmessage = ({ data: { command, ...args } }) => {
+  try {
+    if (!commands[command])
+      return postMessage({ error: 'Unknown command' });
+
+    commands[command](...args);
+
+    postMessage({});
+  } catch (err) {
+    postMessage({ error: err.toString() });
+  }
 };
