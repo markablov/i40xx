b17aa6fe9c9ca1b998a4dee0b72c6d030bff896d
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-28 16:15:59 +0700

    declare ROM-related pins for CPU

--- src/workers/emulator/cpu/pins.js
+++ src/workers/emulator/cpu/pins.js
@@ -2,3 +2,10 @@ class CPUPins {
 }
 
 export default CPUPins;
+
+export const SYNC = Symbol('cpu/pins/SYNC');
+export const CM = Symbol('cpu/pins/CM');
+export const D0 = Symbol('cpu/pins/D0');
+export const D1 = Symbol('cpu/pins/D1');
+export const D2 = Symbol('cpu/pins/D2');
+export const D3 = Symbol('cpu/pins/D3');
