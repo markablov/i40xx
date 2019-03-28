9ff9a8c9e847c4dee92571494780cfb87e772148
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-28 16:16:19 +0700

    implement CPU pin setters

--- src/workers/emulator/cpu/pins.js
+++ src/workers/emulator/cpu/pins.js
@@ -1,4 +1,28 @@
 class CPUPins {
+  _pins = {
+    [SYNC]: 0,
+    [CM]: 0,
+    [D0]: 0,
+    [D1]: 0,
+    [D2]: 0,
+    [D3]: 0
+  };
+
+  /*
+   * Set specified pin to LOW or HIGH (0 or 1)
+   */
+  setPin(pin, value) {
+    this._pins[pin] = value ? 1 : 0;
+  }
+
+  /*
+   * Assign numeric value to set of pins
+   *
+   * For example put 0xA to [D0, D1, D2, D3] pins, and you would got D0 = 0, D1 = 1, D2 = 0, D3 = 1
+   */
+  setDataPins(pins, value) {
+    pins.forEach((pin, idx) => this._pins[pin] = value & (1 << idx));
+  }
 }
 
 export default CPUPins;
