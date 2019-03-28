5ebe08728ea0cb506e381f312d43fedbc075ffb0
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-28 14:24:06 +0700

    skeleton for ROM module

---
+++ src/workers/emulator/rom.js
@@ -0,0 +1,23 @@
+/*
+ * Attach ROM to CPU interface
+ */
+export const attachToCpu = () => {
+};
+
+/*
+ * Check if provided address is in range for ROM
+ */
+export const isAddressValid = () => {
+};
+
+/*
+ * Load dump to ROM
+ */
+export const loadDump = () => {
+};
+
+/*
+ * Main function, that is called every machine cycle and works with internal state and CPU pins
+ */
+export const tick = () => {
+};
