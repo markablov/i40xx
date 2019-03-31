5b2223752b9c9630288a322d9a4b297f37248f81
Mark Ablovatskii <mark.ablov@gmail.com>
2019-03-31 17:49:44 +0700

    implement CPU conveyor w/o instruction execution

--- src/workers/emulator/cpu/cpu.js
+++ src/workers/emulator/cpu/cpu.js
@@ -1,4 +1,4 @@
-import CPUPins, { SYNC } from './pins.js';
+import CPUPins, { SYNC, D0, D1, D2, D3 } from './pins.js';
 
 class CPU {
   registers = {
@@ -20,13 +20,39 @@ class CPU {
   tick() {
     // generate SYNC signal every 8 cycles
     switch (this.syncStep) {
+      // X3 stage
       case 0:
+        if (this.opr !== undefined) {
+          // decode and execute instruction
+        }
         this._pins.setPin(SYNC, 1);
         break;
+      // A1 stage
       case 1:
         this._pins.setPin(SYNC, 0);
+        this._pins.setPinsData([D0, D1, D2, D3], this.registers.pc & 0x000F);
         break;
-      case 8:
+      // A2 stage
+      case 2:
+        this._pins.setPinsData([D0, D1, D2, D3], (this.registers.pc & 0x00F0) >> 4);
+        break;
+      // A3 stage
+      case 3:
+        this._pins.setPinsData([D0, D1, D2, D3], (this.registers.pc & 0x0F00) >> 8);
+        break;
+      // M1 stage
+      case 4:
+        this.opr = this._pins.getPinsData([D0, D1, D2, D3]);
+        break;
+      // M2 stage
+      case 5:
+        this.opa = this._pins.getPinsData([D0, D1, D2, D3]);
+        break;
+      // X1 stage
+      case 6:
+        break;
+      // X2 stage
+      case 7:
         this.syncStep = -1;
         break;
     }
