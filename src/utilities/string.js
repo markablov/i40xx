2efdb57cc878bcf3ae5a582d9d1905f34ba2bf5d
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-02 20:03:43 +0700

    extend utilitary string module to show padded hex numbers

--- src/utilities/string.js
+++ src/utilities/string.js
@@ -1,1 +1,3 @@
 export const pad = (str, padSize) => ('0'.repeat(padSize) + (str || '').toString()).slice(-padSize);
+
+export const padHex = (str, padSize) => pad((str || '').toString(16).toUpperCase(), padSize);
