05f031d7f2af85bec7839ccf29f94bab91aef0d8
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-02 13:12:27 +0700

    implement buildAndRun saga

--- src/redux/sagas/root.js
+++ src/redux/sagas/root.js
@@ -1,4 +1,21 @@
+import { takeLatest, take } from 'redux-saga/effects';
+
+import * as Actions from '../constants.js';
+import { compile } from '../../services/compiler.js';
+import { run } from '../../services/emulator.js';
+
+function* buildAndRun({ payload: sourceCode }) {
+  compile(sourceCode);
+
+  const { payload: { dump, errors } } = yield take(Actions.FINISH_COMPILATION);
+  if (errors && errors.length)
+    return;
+
+  run(dump);
+}
+
 function* rootSaga() {
+  yield takeLatest(Actions.BUILD_AND_RUN, buildAndRun);
 }
 
 export default rootSaga;
