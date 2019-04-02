22ee97be1b7b7161817e3c71f4fce4f0cbca4e99
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-02 12:53:46 +0700

    install redux-saga

---
+++ src/redux/sagas/root.js
@@ -0,0 +1,4 @@
+function* rootSaga() {
+}
+
+export default rootSaga;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducers/root.js';
+import saga from './sagas/root.js';
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(saga);
 
 export default store;
