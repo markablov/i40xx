081cb98316fc4986bb3968fc34fddc784715a55f
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-04 16:16:12 +0700

    wording

--- src/components/Editor/SampleCode.js
+++ src/components/Editor/SampleCode.js
@@ -1,4 +1,4 @@
 export default `  NOP
   INC rr1 // set target address to 0x01     
-  FIN r1  // load data to rr2 / rr3, it should be 8-bit bytecode for INC instruction
+  FIN r1  // load data to r1, it should be 8-bit bytecode for INC instruction
 `;
