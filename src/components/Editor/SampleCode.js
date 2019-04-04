1c56e977511aa5f434458632d2dbf3f1df7dcb56
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-04 15:47:37 +0700

    update sample code to test FIN instruction

--- src/components/Editor/SampleCode.js
+++ src/components/Editor/SampleCode.js
@@ -1,3 +1,4 @@
-export default `  LDM 9
-  XCH rr0`
-;
+export default `  NOP
+  INC rr1 // set target address to 0x01     
+  FIN r1  // load data to rr2 / rr3, it should be 8-bit bytecode for INC instruction
+`;
