07001b4bdf23cfacda395cda8c8a6c06bca5d3dc
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-12 17:34:48 +0700

    add toHex() utilitary method to string module

--- src/utilities/string.js
+++ src/utilities/string.js
@@ -1,3 +1,5 @@
+export const toHex = str => (str == null ? '' : str).toString(16).toUpperCase();
+
 export const pad = (str, padSize) => ('0'.repeat(padSize) + (str || '').toString()).slice(-padSize);
 
-export const padHex = (str, padSize) => pad((str || '').toString(16).toUpperCase(), padSize);
+export const padHex = (str, padSize) => pad(toHex(str), padSize);
