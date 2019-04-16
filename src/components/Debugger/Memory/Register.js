3f62add7758eec2d6b0e52adc22c3fa60355d6ef
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-16 10:57:16 +0700

    display RAM register content more compact + shade zero bytes

--- src/components/Debugger/Memory/Register.js
+++ src/components/Debugger/Memory/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 
 import FramedBox from '../../UI/FramedBox/FramedBox.js';
 import { toHex } from '../../../utilities/string.js';
@@ -7,10 +8,12 @@ import './Register.css';
 
 const Register = ({ data, index }) => (
   <FramedBox title={`Register #${index}`} narrow={true}>
-    <span>{data.main.slice(0, 10).map(toHex).join(' ')}</span>
+    { data.main.slice(0, 10).map(toHex).map((x, i) => <span key={`reg-${index}-char-${i}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
     <br/>
-    <span>{data.main.slice(10).map(toHex).join(' ') + ' '}</span>
-    <span className="statusCharacters">{data.status.map(toHex).join(' ')}</span>
+    { data.main.slice(10).map(toHex).map((x, i) => <span key={`reg-${index}-char-${i + 10}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
+    <span className="statusCharacters">
+      { data.status.map(toHex).map((x, i) => <span key={`reg-${index}-status-${i}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
+    </span>
   </FramedBox>
 );
 
