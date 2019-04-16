17bfa2dbd5861eecc0dc4f72d0bcd172636d7d8e
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-16 09:14:54 +0700

    add dropdown to select memory bank on Memory tab

--- src/components/Debugger/Memory/Memory.js
+++ src/components/Debugger/Memory/Memory.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Field, Control, Select } from 'react-bulma-components/lib/components/form';
 
 class Memory extends Component {
+  handleBankSelect = () => {
+  };
+
   render(){
     return (
       <>
+        <Field>
+          <Control>
+            <Select onChange={this.handleBankSelect}>
+            </Select>
+          </Control>
+        </Field>
       </>
     );
   }
