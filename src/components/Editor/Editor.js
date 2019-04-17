e529cfe24697914775b4ff7e274d0802d144e1f3
Mark Ablovatskii <mark.ablov@gmail.com>
2019-04-17 08:25:23 +0700

    perform work at getDerivedStateFromProps, not in shouldComponentUpdate

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -12,6 +12,7 @@ import SampleCode from './SampleCode.js';
 import setEditorRef from '../../redux/actions/setEditorRef.js';
 import setBreakpoints from '../../redux/actions/setBreakpoints.js';
 import { step, continueExec } from '../../services/emulator.js';
+import store from '../../redux/store.js';
 
 import './Editor.css';
 
@@ -26,9 +27,11 @@ class Editor extends Component {
   }
 
   setupROMOffsets(editor, session) {
-    const offsetCalculator = this.offsetCalculator = new OffsetCalculator(editor);
+    const offsetCalculator = new OffsetCalculator(editor);
     const gutterRenderer = new GutterRenderer(editor, offsetCalculator);
     const bankSeparatorRenderer = new BankSeparatorRenderer(editor, offsetCalculator);
+
+    this.setState({ offsetCalculator });
 
     // we want to show ROM offset for instructions, so need to change
     // gutter with line numbers to custom renderer
@@ -46,7 +49,7 @@ class Editor extends Component {
     });
   }
 
-  isDebugMode(emulator) {
+  static isDebugMode(emulator) {
     return emulator && emulator.running && emulator.mode === 'debug';
   }
 
@@ -64,7 +67,7 @@ class Editor extends Component {
       name: 'emulatorStep',
       bindKey: { win: 'F10', mac: 'F10' },
       exec: () => {
-        if (this.isDebugMode(this.props.emulator))
+        if (Editor.isDebugMode(this.props.emulator))
           step();
       },
       scrollIntoView: 'cursor',
@@ -76,7 +79,7 @@ class Editor extends Component {
       name: 'emulatorContinue',
       bindKey: { win: 'F9', mac: 'F9' },
       exec: () => {
-        if (this.isDebugMode(this.props.emulator))
+        if (Editor.isDebugMode(this.props.emulator))
           continueExec();
       },
       scrollIntoView: 'cursor',
@@ -95,11 +98,11 @@ class Editor extends Component {
 
     editor.on('guttermousedown', e => {
       // allow to edit breakpoints only during debug
-      if (!this.isDebugMode(this.props.emulator))
+      if (!Editor.isDebugMode(this.props.emulator))
         return e.preventDefault();
 
       const row = e.getDocumentPosition().row;
-      const offset = this.offsetCalculator.offset(row);
+      const offset = this.state.offsetCalculator.offset(row);
       if (session.getBreakpoints()[row]) {
         session.clearBreakpoint(row);
         delete this.props.breakpoints[offset];
@@ -116,48 +119,49 @@ class Editor extends Component {
     this.setupROMOffsets(editor, session);
   }
 
-  shouldComponentUpdate({ compilerErrors, emulator }) {
-    // ignore all updates, except emulator and compilerErrors
-    if (emulator === this.props.emulator && compilerErrors === this.props.compilerErrors)
-      return false;
+  static getDerivedStateFromProps({ compilerErrors, emulator, breakpoints }, { editorRef, executingLine, offsetCalculator }) {
+    const editor = editorRef.current && editorRef.current.editor;
+    if (!editor)
+      return null;
 
-    // synchronize ACE editor state with redux state
-    // because ACE editor is not react component we should avoid re-rendering and
-    // perform all changes via ACE Editor API
-    if (this.editor) {
-      // show compilation errors
-      if (compilerErrors && compilerErrors.length) {
-        session.setAnnotations(compilerErrors.map(error => ({ ...error, type: 'error' })));
-        return false;
-      }
+    // show compilation errors
+    if (compilerErrors && compilerErrors.length) {
+      session.setAnnotations(compilerErrors.map(error => ({ ...error, type: 'error' })));
+      return null;
+    }
 
-      const debugMode = this.isDebugMode(emulator);
-      const session = this.editor.getSession();
-      const { executingLine } = this.state;
+    const debugMode = Editor.isDebugMode(emulator);
+    const session = editor.getSession();
 
-      // during debug editor should be on read-only state
-      const currentReadOnly = this.editor.getReadOnly();
-      const expectedReadOnly = debugMode === true;
-      if (currentReadOnly !== expectedReadOnly)
-        this.editor.setReadOnly(expectedReadOnly);
+    // during debug, editor should be on read-only state
+    const currentReadOnly = editor.getReadOnly();
+    const expectedReadOnly = debugMode === true;
+    if (currentReadOnly !== expectedReadOnly)
+      editor.setReadOnly(expectedReadOnly);
 
-      if (debugMode) {
-        const row = this.offsetCalculator.row(emulator.registers.pc);
-        if (!executingLine || executingLine.row !== row) {
-          if (executingLine)
-            session.removeMarker(executingLine.id);
-          this.setState({ executingLine: { row, id: session.highlightLines(row, row).id } });
-        }
-      } else {
-        session.clearBreakpoints();
-        this.props.setBreakpoints({});
-        if (executingLine) {
+    if (debugMode) {
+      const row = offsetCalculator.row(emulator.registers.pc);
+      if (!executingLine || executingLine.row !== row) {
+        if (executingLine)
           session.removeMarker(executingLine.id);
-          this.setState({ executingLine: null });
-        }
+        const highlighted = session.highlightLines(row, row);
+        return { executingLine: { row, id: highlighted.id } };
+      }
+    } else {
+      session.clearBreakpoints();
+      if (breakpoints && Object.keys(breakpoints).length)
+        store.dispatch(setBreakpoints({}));
+
+      if (executingLine) {
+        session.removeMarker(executingLine.id);
+        return { executingLine: null };
       }
     }
 
+    return null;
+  }
+
+  shouldComponentUpdate() {
     // Editor have no props or state, except external compilerErrors
     // so we don't want ever to re-render component
     return false;
