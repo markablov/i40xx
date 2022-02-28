e7a25d2c7bd82a6e7728b0138abb6888841ae4dd
Mark Ablovatskii <mark.ablov@gmail.com>
2022-02-28 18:21:32 +0700

    eslint fixes #4

--- src/components/Editor/OffsetCalculator/OffsetCalculator.js
+++ src/components/Editor/OffsetCalculator/OffsetCalculator.js
@@ -1,52 +1,54 @@
 import instructionLength from './InstructionLength.js';
 
 class OffsetCalculator {
-  _instructions = [{ len: 0, offset: 0 }];
+  #instructions = [{ len: 0, offset: 0 }];
 
   constructor(editor) {
     this.editor = editor;
   }
 
   offset(row) {
-    return this._instructions[row].offset;
+    return this.#instructions[row].offset;
   }
 
   row(offset) {
-    return this._instructions.findIndex(instruction => instruction.offset === offset && instruction.len > 0);
+    return this.#instructions.findIndex((instruction) => instruction.offset === offset && instruction.len > 0);
   }
 
   all() {
-    return this._instructions;
+    return this.#instructions;
   }
 
-  update({ action, lines, start: { row: startRow }, end: { row: endRow } }){
+  update({ action, end: { row: endRow }, lines, start: { row: startRow } }) {
     const doc = this.editor.getSession().getDocument();
     let changed = false;
 
     // calculate length of updated instructions
     if (startRow === endRow) {
       const newInstrLen = instructionLength(doc.getLine(startRow));
-      if (newInstrLen !== this._instructions[startRow].len) {
+      if (newInstrLen !== this.#instructions[startRow].len) {
         changed = true;
-        this._instructions[startRow].len = newInstrLen;
+        this.#instructions[startRow].len = newInstrLen;
       }
     } else {
       changed = true;
       if (action === 'insert') {
-        this._instructions.splice(startRow + 1, 0, ...Array.from(Array(lines.length - 1), () => ({})));
-        for (let i = startRow, end = startRow + lines.length; i < end; i++)
-          this._instructions[i].len = instructionLength(doc.getLine(i));
+        this.#instructions.splice(startRow + 1, 0, ...Array.from(Array(lines.length - 1), () => ({})));
+        for (let i = startRow, end = startRow + lines.length; i < end; i++) {
+          this.#instructions[i].len = instructionLength(doc.getLine(i));
+        }
       } else if (action === 'remove') {
-        this._instructions.splice(startRow + 1, endRow - startRow);
-        this._instructions[startRow].len = instructionLength(doc.getLine(startRow));
+        this.#instructions.splice(startRow + 1, endRow - startRow);
+        this.#instructions[startRow].len = instructionLength(doc.getLine(startRow));
       }
     }
 
     // if any instruction length has been updated or instructions has been added/removed
     // then we need to update offsets for following instructions
     if (changed) {
-      for (let i = startRow + 1, end = this._instructions.length; i < end; i++)
-        this._instructions[i].offset = this._instructions[i - 1].offset + this._instructions[i - 1].len;
+      for (let i = startRow + 1, end = this.#instructions.length; i < end; i++) {
+        this.#instructions[i].offset = this.#instructions[i - 1].offset + this.#instructions[i - 1].len;
+      }
     }
 
     return changed;
