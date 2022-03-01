35e17a1f3feeb1db7ef343b95063ca74ab84aaf1
Mark Ablovatskii <mark.ablov@gmail.com>
2022-03-01 11:04:35 +0700

    eslint fixes #14

--- src/components/Debugger/Memory/Register.js
+++ src/components/Debugger/Memory/Register.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import cx from 'classnames';
 
@@ -6,15 +7,26 @@ import { toHex } from '../../../utilities/string.js';
 
 import './Register.css';
 
-const Register = ({ data, index }) => (
-  <FramedBox title={`Register #${index}`} narrow={true}>
-    { data.main.slice(0, 10).map(toHex).map((x, i) => <span key={`reg-${index}-char-${i}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
-    <br/>
-    { data.main.slice(10).map(toHex).map((x, i) => <span key={`reg-${index}-char-${i + 10}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
-    <span className="statusCharacters">
-      { data.status.map(toHex).map((x, i) => <span key={`reg-${index}-status-${i}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
-    </span>
-  </FramedBox>
-);
+function Register({ data, index }) {
+  return (
+    <FramedBox narrow title={`Register #${index}`}>
+      { data.main.slice(0, 10).map(toHex).map((x, i) => <span key={`reg-${index}-char-${i}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
+      <br />
+      { data.main.slice(10).map(toHex).map((x, i) => <span key={`reg-${index}-char-${i + 10}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
+      <span className="statusCharacters">
+        { data.status.map(toHex).map((x, i) => <span key={`reg-${index}-status-${i}`} className={cx('character', { zero: +x === 0 })}>{x}</span>) }
+      </span>
+    </FramedBox>
+  );
+}
+
+Register.propTypes = {
+  data: PropTypes.shape({
+    main: PropTypes.arrayOf(PropTypes.number),
+    status: PropTypes.arrayOf(PropTypes.number),
+  }).isRequired,
+
+  index: PropTypes.number.isRequired,
+};
 
 export default Register;
