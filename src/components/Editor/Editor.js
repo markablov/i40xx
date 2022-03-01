32ddfa1b40e4dbc91a88a1477918b7eec02d97ac
Mark Ablovatskii <mark.ablov@gmail.com>
2022-03-01 11:53:58 +0700

    eslint fixes #17

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AceEditor from 'react-ace';
@@ -10,20 +11,40 @@ import OffsetCalculator from './OffsetCalculator/OffsetCalculator.js';
 import AssemblyMode from './AssemblyMode/AssemblyMode.js';
 import BankSeparatorRenderer from './BankSeparatorRenderer.js';
 import SampleCode from './SampleCode.js';
-import setEditorRef from '../../redux/actions/setEditorRef.js';
-import setBreakpoints from '../../redux/actions/setBreakpoints.js';
+import setEditorRefAction from '../../redux/actions/setEditorRef.js';
+import setBreakpointsAction from '../../redux/actions/setBreakpoints.js';
 import { stepInto, stepOver, continueExec } from '../../services/emulator.js';
 
 import './Editor.css';
 
 class Editor extends Component {
+  static isDebugMode(emulator) {
+    return emulator && emulator.running && emulator.mode === 'debug';
+  }
+
+  static #load = () => localStorage.getItem('source_code');
+
+  static #save = () => localStorage.setItem('source_code', this.editor.getValue());
+
   state = {
+    editorRef: React.createRef(),
     executingLine: undefined,
-    editorRef: React.createRef()
   };
 
-  get editor(){
-    return this.state.editorRef.current.editor;
+  componentDidMount() {
+    const { setEditorRef } = this.props;
+
+    setEditorRef(this.editor);
+    this.setupEditor();
+    this.editor.setValue(Editor.#load() || SampleCode, -1);
+  }
+
+  static handleSave = () => Editor.#save();
+
+  shouldComponentUpdate() {
+    // Editor have no props or state, except external compilerErrors
+    // so we don't want ever to re-render component
+    return false;
   }
 
   setupROMOffsets(editor, session) {
@@ -40,7 +61,7 @@ class Editor extends Component {
     editor.on('changeSelection', () => gutterRenderer.update());
     gutterRenderer.update();
 
-    session.on('change', delta => {
+    session.on('change', (delta) => {
       if (offsetCalculator.update(delta)) {
         bankSeparatorRenderer.updateSeparatorPositions();
         gutterRenderer.update();
@@ -49,81 +70,28 @@ class Editor extends Component {
     });
   }
 
-  static isDebugMode(emulator) {
-    return emulator && emulator.running && emulator.mode === 'debug';
-  }
-
-  setupShortcuts(editor) {
-    // ACE editor already have Ctrl+D to remove line, but i prefer Ctrl+Y
-    editor.commands.addCommand({
-      name: 'removeline2',
-      bindKey: { win: 'Ctrl-Y', mac: 'Command-Y' },
-      exec: editor => editor.removeLines(),
-      scrollIntoView: 'cursor',
-      multiSelectAction: 'forEachLine'
-    });
-
-    editor.commands.addCommand({
-      name: 'emulatorStepInto',
-      bindKey: { win: 'F11', mac: 'F11' },
-      exec: () => {
-        if (Editor.isDebugMode(this.props.emulator))
-          stepInto();
-      },
-      scrollIntoView: 'cursor',
-      multiSelectAction: 'forEach',
-      readOnly: true
-    });
-
-    editor.commands.addCommand({
-      name: 'emulatorStepOver',
-      bindKey: { win: 'F10', mac: 'F10' },
-      exec: () => {
-        if (Editor.isDebugMode(this.props.emulator))
-          stepOver();
-      },
-      scrollIntoView: 'cursor',
-      multiSelectAction: 'forEach',
-      readOnly: true
-    });
-
-    editor.commands.addCommand({
-      name: 'emulatorContinue',
-      bindKey: { win: 'F9', mac: 'F9' },
-      exec: () => {
-        if (Editor.isDebugMode(this.props.emulator))
-          continueExec();
-      },
-      multiSelectAction: 'forEach',
-      readOnly: true
-    });
-
-    editor.commands.addCommand({
-      name: 'save',
-      bindKey: { win: 'Ctrl-S', mac: 'Cmd-S' },
-      exec: () => this._save(),
-      multiSelectAction: 'forEach'
-    });
-  }
-
   setupEditor() {
-    const editor = this.editor, session = editor.getSession();
+    const { editor } = this;
+    const session = editor.getSession();
 
     editor.$blockScrolling = Infinity;
 
     session.setMode(new AssemblyMode());
     session.setTabSize(2);
 
-    editor.on('guttermousedown', e => {
-      const row = e.getDocumentPosition().row;
-      const offset = this.state.offsetCalculator.offset(row);
+    editor.on('guttermousedown', (e) => {
+      const { offsetCalculator } = this.state;
+      const { breakpoints, setBreakpoints } = this.props;
+
+      const { row } = e.getDocumentPosition();
+      const offset = offsetCalculator.offset(row);
       if (session.getBreakpoints()[row]) {
         session.clearBreakpoint(row);
-        delete this.props.breakpoints[offset];
-        this.props.setBreakpoints(this.props.breakpoints);
+        delete breakpoints[offset];
+        setBreakpoints(breakpoints);
       } else {
         session.setBreakpoint(row);
-        this.props.setBreakpoints({ ...this.props.breakpoints, [offset]: true });
+        setBreakpoints({ ...breakpoints, [offset]: true });
       }
 
       return e.preventDefault();
@@ -133,16 +101,79 @@ class Editor extends Component {
     this.setupROMOffsets(editor, session);
   }
 
+  setupShortcuts(editor) {
+    // ACE editor already have Ctrl+D to remove line, but i prefer Ctrl+Y
+    editor.commands.addCommand({
+      bindKey: { mac: 'Command-Y', win: 'Ctrl-Y' },
+      exec: () => editor.removeLines(),
+      multiSelectAction: 'forEachLine',
+      name: 'removeline2',
+      scrollIntoView: 'cursor',
+    });
+
+    editor.commands.addCommand({
+      bindKey: { mac: 'F11', win: 'F11' },
+      exec: () => {
+        const { emulator } = this.props;
+
+        if (Editor.isDebugMode(emulator)) {
+          stepInto();
+        }
+      },
+      multiSelectAction: 'forEach',
+      name: 'emulatorStepInto',
+      readOnly: true,
+      scrollIntoView: 'cursor',
+    });
+
+    editor.commands.addCommand({
+      bindKey: { mac: 'F10', win: 'F10' },
+      exec: () => {
+        const { emulator } = this.props;
+
+        if (Editor.isDebugMode(emulator)) {
+          stepOver();
+        }
+      },
+      multiSelectAction: 'forEach',
+      name: 'emulatorStepOver',
+      readOnly: true,
+      scrollIntoView: 'cursor',
+    });
+
+    editor.commands.addCommand({
+      bindKey: { mac: 'F9', win: 'F9' },
+      exec: () => {
+        const { emulator } = this.props;
+
+        if (Editor.isDebugMode(emulator)) {
+          continueExec();
+        }
+      },
+      multiSelectAction: 'forEach',
+      name: 'emulatorContinue',
+      readOnly: true,
+    });
+
+    editor.commands.addCommand({
+      bindKey: { mac: 'Cmd-S', win: 'Ctrl-S' },
+      exec: () => Editor.#save(),
+      multiSelectAction: 'forEach',
+      name: 'save',
+    });
+  }
+
   static getDerivedStateFromProps({ compilerErrors, emulator }, { editorRef, executingLine, offsetCalculator }) {
     const editor = editorRef.current && editorRef.current.editor;
-    if (!editor)
+    if (!editor) {
       return null;
+    }
 
     const session = editor.getSession();
 
     // show compilation errors
     if (compilerErrors && compilerErrors.length) {
-      session.setAnnotations(compilerErrors.map(error => ({ ...error, type: 'error' })));
+      session.setAnnotations(compilerErrors.map((error) => ({ ...error, type: 'error' })));
       return null;
     }
 
@@ -151,59 +182,75 @@ class Editor extends Component {
     // during debug, editor should be on read-only state
     const currentReadOnly = editor.getReadOnly();
     const expectedReadOnly = debugMode === true;
-    if (currentReadOnly !== expectedReadOnly)
+    if (currentReadOnly !== expectedReadOnly) {
       editor.setReadOnly(expectedReadOnly);
+    }
 
     if (debugMode) {
       const row = offsetCalculator.row(emulator.registers.pc);
       if (!executingLine || executingLine.row !== row) {
-        if (executingLine)
+        if (executingLine) {
           session.removeMarker(executingLine.id);
+        }
         const highlighted = session.highlightLines(row, row);
         editor.moveCursorTo(row, 0);
         editor.clearSelection();
-        if (!editor.isRowFullyVisible(row))
+        if (!editor.isRowFullyVisible(row)) {
           editor.scrollToLine(row);
-        return { executingLine: { row, id: highlighted.id } };
+        }
+        return { executingLine: { id: highlighted.id, row } };
       }
-    } else {
-      if (executingLine) {
-        session.removeMarker(executingLine.id);
-        return { executingLine: null };
-      }
+    } else if (executingLine) {
+      session.removeMarker(executingLine.id);
+      return { executingLine: null };
     }
 
     return null;
   }
 
-  shouldComponentUpdate() {
-    // Editor have no props or state, except external compilerErrors
-    // so we don't want ever to re-render component
-    return false;
+  get editor() {
+    const { editorRef: { current: { editor } } } = this.state;
+    return editor;
   }
 
-  _save = () => localStorage.setItem('source_code', this.editor.getValue());
+  render() {
+    const { editorRef } = this.state;
 
-  _load = () => localStorage.getItem('source_code');
-
-  componentDidMount() {
-    this.props.setEditorRef(this.editor);
-    this.setupEditor();
-    this.editor.setValue(this._load() || SampleCode, -1);
-  }
-
-  handleSave = () => this._save();
-
-  render(){
     return (
       <>
-        <AceEditor mode="text" theme="monokai" name="editor" width="auto" ref={this.state.editorRef} />
+        <AceEditor ref={editorRef} mode="text" name="editor" theme="monokai" width="auto" />
         <div className="buttons">
-          <Button color="success" onClick={this.handleSave}>Save</Button>
+          <Button color="success" onClick={Editor.handleSave}>Save</Button>
         </div>
       </>
     );
   }
 }
 
-export default connect(({ compilerErrors, emulator, breakpoints }) => ({ compilerErrors, emulator, breakpoints }), { setEditorRef, setBreakpoints })(Editor);
+Editor.propTypes = {
+  breakpoints: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.bool)).isRequired,
+
+  compilerErrors: PropTypes.arrayOf(PropTypes.shape({
+    column: PropTypes.number,
+    row: PropTypes.number,
+    text: PropTypes.string,
+  })).isRequired,
+
+  emulator: PropTypes.shape({
+    mode: PropTypes.string,
+    registers: PropTypes.shape({
+      pc: PropTypes.number,
+    }),
+    running: PropTypes.bool,
+  }).isRequired,
+
+  setBreakpoints: PropTypes.func.isRequired,
+  setEditorRef: PropTypes.func.isRequired,
+};
+
+const mapFn = connect(
+  ({ breakpoints, compilerErrors, emulator }) => ({ breakpoints, compilerErrors, emulator }),
+  { setBreakpoints: setBreakpointsAction, setEditorRef: setEditorRefAction },
+);
+
+export default mapFn(Editor);
