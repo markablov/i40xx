babe3c0f5a359c6bd7cad2d8479fd8f96a87555c
Mark Ablovatskii <mark.ablov@gmail.com>
2022-03-01 10:47:14 +0700

    eslint fixes #9

--- src/components/UI/FramedBox/FramedBox.js
+++ src/components/UI/FramedBox/FramedBox.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 import './FramedBox.css';
 
-const FramedBox = ({ children, title, narrow }) => (
-  <div className={cx('framedBox', { 'is-inline-block': narrow !== undefined })}>
-    <h1><span>{title}</span></h1>
-    {children}
-  </div>
-);
+function FramedBox({ children, narrow, title }) {
+  return (
+    <div className={cx('framedBox', { 'is-inline-block': narrow !== undefined })}>
+      <h1><span>{title}</span></h1>
+      {children}
+    </div>
+  );
+}
+
+FramedBox.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element), PropTypes.element]).isRequired,
+  narrow: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+};
+
+FramedBox.defaultProps = {
+  narrow: false,
+};
 
 export default FramedBox;
