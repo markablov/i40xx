a8d1eb11d73e1d3655f41a6bf63dbcf561cd8de5
Mark Ablovatskii <mark.ablov@gmail.com>
2022-05-03 14:28:49 +0700

    use scss version of bulma rather than precompiled

--- src/screens/Root.js
+++ src/screens/Root.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Box, Columns, Container, Section } from 'react-bulma-components';
 
-import 'bulma/css/bulma.min.css';
-
 import Editor from '../components/Editor/Editor.js';
 import Debugger from '../components/Debugger/Debugger.js';
 import Footer from '../components/Footer.js';
 import BusyOverlay from '../components/BusyOverlay.js';
 
-import './Root.css';
+import './Root.scss';
 
 function Root() {
   return (
