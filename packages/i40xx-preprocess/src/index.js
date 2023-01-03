114a5bc4f532ee5ed9ad059e2a9f909a3088549c
Mark Ablovatskii <mark.ablov@gmail.com>
2023-01-03 14:50:24 +0700

    [preprocess] basic support for macroses

--- packages/i40xx-preprocess/src/index.js
+++ packages/i40xx-preprocess/src/index.js
@@ -4,11 +4,20 @@ import * as path from 'node:path';
 import { extractDirectives } from './workers/directives.js';
 
 /*
+ * Transform single file
+ */
+const transformSourceCode = (sourceCode, { defines }) => sourceCode.replace(
+  /\$(\w+)/g,
+  (match, identifier) => defines.get(identifier),
+);
+
+/*
  * Opens assembly source code and returns preprocessed listing
  */
 export function preprocessFile(filePath) {
   const unvisited = [filePath];
   const sourceCodeFiles = new Map();
+  const defines = new Map();
 
   while (unvisited.length) {
     const currentPath = unvisited.shift();
@@ -16,14 +25,22 @@ export function preprocessFile(filePath) {
     const { directives, sourceCode } = extractDirectives(codeRaw);
     sourceCodeFiles.set(currentPath, sourceCode);
 
-    const includesFromFile = directives.map((directive) => directive.match(/^%include "(.+)"$/)?.[1]).filter(Boolean);
-    for (const includePath of includesFromFile) {
-      const fullPathForInclude = path.resolve(path.dirname(currentPath), includePath);
-      if (!sourceCodeFiles.has(fullPathForInclude)) {
-        unvisited.push(fullPathForInclude);
+    for (const directive of directives) {
+      const [, includePath] = directive.match(/^%include "(.+)"$/) || [];
+      if (includePath) {
+        const fullPathForInclude = path.resolve(path.dirname(currentPath), includePath);
+        if (!sourceCodeFiles.has(fullPathForInclude)) {
+          unvisited.push(fullPathForInclude);
+        }
+        continue;
+      }
+
+      const [, identifier, replacement] = directive.match(/^%define (\w+)\s+(\w+)$/) || [];
+      if (identifier && replacement) {
+        defines.set(identifier, replacement);
       }
     }
   }
 
-  return [...sourceCodeFiles.values()].join('\n');
+  return [...sourceCodeFiles.values()].map((sourceCode) => transformSourceCode(sourceCode, { defines })).join('\n');
 }
