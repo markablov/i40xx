0cf57dbf62c803deeaef7d4fe9342ccedb675f4f
Mark Ablovatskii <mark.ablov@gmail.com>
2023-01-05 14:20:47 +0700

    [prog-pi-digits] include multiplication tables into initial RAM dump

--- packages/i40xx-program-pi-digits/src/submodules/tests/data/generateRamDumpWithLookupTables.js
+++ packages/i40xx-program-pi-digits/src/submodules/tests/data/generateRamDumpWithLookupTables.js
@@ -2,20 +2,38 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+const addMulTableEntry = (ramDump, a, b, bankNo, lowCharNo) => {
+  ramDump[bankNo][a].status[lowCharNo] = (a * b) & 0xF;
+  ramDump[bankNo][a].status[lowCharNo + 1] = (a * b) >> 4;
+};
+
 (function main() {
   const ramDump = Array.from(
     Array(8),
     () => Array.from(Array(16), () => ({ main: Array(16).fill(0), status: Array(4).fill(0) })),
   );
 
-  // shift tables
   for (let value = 0; value < 16; value++) {
+    // shift tables
     ramDump[0][value].status[0] = (value << 1) & 0xF;
     ramDump[0][value].status[1] = (value << 3) & 0xF;
     ramDump[0][value].status[2] = (value >> 1) & 0xF;
     ramDump[0][value].status[3] = (value >> 3) & 0xF;
     ramDump[1][value].status[0] = (value << 2) & 0xF;
     ramDump[1][value].status[1] = (value >> 2) & 0xF;
+
+    // mul tables
+    addMulTableEntry(ramDump, value, 0x3, 1, 2);
+    addMulTableEntry(ramDump, value, 0x5, 2, 0);
+    addMulTableEntry(ramDump, value, 0x6, 2, 2);
+    addMulTableEntry(ramDump, value, 0x7, 4, 0);
+    addMulTableEntry(ramDump, value, 0x9, 4, 2);
+    addMulTableEntry(ramDump, value, 0xA, 3, 0);
+    addMulTableEntry(ramDump, value, 0xB, 3, 2);
+    addMulTableEntry(ramDump, value, 0xC, 5, 0);
+    addMulTableEntry(ramDump, value, 0xD, 5, 2);
+    addMulTableEntry(ramDump, value, 0xE, 6, 0);
+    addMulTableEntry(ramDump, value, 0xF, 6, 2);
   }
 
   const dumpPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), './ramWithLookupTables.json');
