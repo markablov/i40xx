91f912ef0284dd8fee376cf871283b058aa8b12b
Mark Ablovatskii <mark.ablov@gmail.com>
2023-01-06 14:14:14 +0700

    [prog-pi-digits] add faster version of specific multi-word multiplication with tests

--- packages/i40xx-program-pi-digits/src/utilities/numbers.js
+++ packages/i40xx-program-pi-digits/src/utilities/numbers.js
@@ -14,10 +14,28 @@ export const hwNumberToHex = (chars) => (
 );
 
 /*
+ * Converts number stored inside RAM register to regular number
+ *
+ * Data is stored as little-endian numbers [least significant digit, ..., most significant digit]
+ */
+export const hwNumberToNum = (chars) => (
+  parseInt([...chars].reverse().map((char) => char.toString(16)).join(''), 16)
+);
+
+/*
  * Convert hexadecimal string to array of digits, that could be stored inside RAM register
  *
  * Data is stored as little-endian numbers [least significant digit, ..., most significant digit]
  */
 export const hexToHWNumber = (hex) => (
   hex.substring(2).split('').reverse().map((char) => parseInt(char, 16))
+);
+
+/*
+ * Convert number to array of digits, that could be stored inside RAM register
+ *
+ * Data is stored as little-endian numbers [least significant digit, ..., most significant digit]
+ */
+export const numToHWNumber = (num) => (
+  num.toString(16).split('').reverse().map((char) => parseInt(char, 16))
 );
