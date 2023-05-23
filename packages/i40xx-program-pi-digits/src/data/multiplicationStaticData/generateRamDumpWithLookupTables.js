586bcdbecdf4772524216ef6227677a4b22c145f
Mark Ablovatskii <mark.ablov@gmail.com>
2023-05-23 10:38:10 +0700

    [prog-pi-digits] store segmentSize (and make it always even) for prime numbers in main memory + don't generate exponentiation tables

--- packages/i40xx-program-pi-digits/src/data/multiplicationStaticData/generateRamDumpWithLookupTables.js
+++ packages/i40xx-program-pi-digits/src/data/multiplicationStaticData/generateRamDumpWithLookupTables.js
@@ -2,8 +2,6 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { numToHWNumber } from '#utilities/numbers.js';
-
 const addMulTableEntry = (ramDump, a, b, bankNo, lowCharNo) => {
   ramDump[bankNo][a].status[lowCharNo] = (a * b) & 0xF;
   ramDump[bankNo][a].status[lowCharNo + 1] = (a * b) >> 4;
@@ -30,28 +28,6 @@ const addMulTableEntry = (ramDump, a, b, bankNo, lowCharNo) => {
     addMulTableEntry(ramDump, value, 0xE, 6, 0);
     addMulTableEntry(ramDump, value, 0xF, 6, 2);
   }
-
-  // tables for exponentiation
-  ramDump[7][0x6].main = [
-    ...numToHWNumber(0x11 ** 2, 4),
-    ...numToHWNumber(0x13 ** 2, 4),
-    ...numToHWNumber(0x3 ** 5, 4),
-    ...numToHWNumber(0x17 ** 2, 4),
-  ];
-
-  ramDump[7][0x7].main = [
-    ...numToHWNumber(0, 4),
-    ...numToHWNumber(0x3 ** 3, 4),
-    ...numToHWNumber(0x5 ** 3, 4),
-    ...numToHWNumber(0x7 ** 3, 4),
-  ];
-
-  ramDump[7][0xB].main = [
-    ...numToHWNumber(0x3 ** 7, 4),
-    ...numToHWNumber(0x3 ** 4, 4),
-    ...numToHWNumber(0x5 ** 4, 4),
-    ...numToHWNumber(0x3 ** 6, 4),
-  ];
 
   const dumpPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), './ramWithLookupTables.json');
   fs.writeFileSync(dumpPath, JSON.stringify(ramDump, undefined, 2));
