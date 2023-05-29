8de6fab12d957f6e695f430d4d5b9330262e0af5
Mark Ablovatskii <mark.ablov@gmail.com>
2023-05-29 13:08:27 +0700

    [prog-pi-digits] fix test for updateDigits() routine with correct placement of chunkNo variable into memory

--- packages/i40xx-program-pi-digits/src/tests/updateDigits/memory.js
+++ packages/i40xx-program-pi-digits/src/tests/updateDigits/memory.js
@@ -9,7 +9,8 @@ export function initMemoryWithInput(memory, input) {
   const startingDigitsPositionHW = hexToHWNumber(startingPiDigitsPosition);
   const numN = parseInt(N, 16);
 
-  writeValueToStatusChars(numToHWNumber(0x100 - chunks.length), memory, VARIABLES.STATUS_MEM_VARIABLE_CHUNKS_COUNT_NEG);
+  const [chunksCountL, chunksCountH] = numToHWNumber(0x100 - chunks.length);
+  writeValueToStatusChars([0, 0, chunksCountL, chunksCountH], memory, VARIABLES.STATUS_MEM_VARIABLE_CHUNKS_COUNT_NEG);
   writeValueToStatusChars(startingDigitsPositionHW, memory, VARIABLES.STATUS_MEM_VARIABLE_STARTING_PI_DIGITS_POSITION);
   writeValueToStatusChars(hexToHWNumber(a), memory, VARIABLES.STATUS_MEM_VARIABLE_CURRENT_PRIME);
   writeValueToStatusChars(numToHWNumber(0x10000 - (numN + 1)), memory, VARIABLES.STATUS_MEM_VARIABLE_N_NEG);
